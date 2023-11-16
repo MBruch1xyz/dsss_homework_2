f7556463c5e779cdba24a8c84d27d2ecec81dfe3
MBruch1 <110245266+MBruch1@users.noreply.github.com>
2023-11-16 13:14:22 +0100

    new files

---
+++ math_quiz/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    entry_points={
+        'console_scripts': [
+            'math_quiz = math_quiz.math_quiz:main',
+        ],
+    },
+)
