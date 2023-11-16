d08826204741d9eae8c0e91bc2047f332a1e64b3
MBruch1 <110245266+MBruch1@users.noreply.github.com>
2023-11-16 12:06:50 +0100

    code cleanup

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,84 @@
 import random
 
-
-def function_A(min, max):
+def random_integer(min_value, max_value):
     """
-    Random integer.
+    Generates a random integer within the specified range (inclusive).
+
+    Parameters:
+    - min_value (int): The minimum value of the range.
+    - max_value (int): The maximum value of the range.
+
+    Returns:
+    int: A random integer within the specified range.
     """
-    return random.randint(min, max)
+    return random.randint(min_value, max_value)
 
+def random_operator():
+    """
+    Selects a random arithmetic operator from the set ('+', '-', '*').
 
-def function_B():
+    Returns:
+    str: A randomly chosen operator.
+    """
     return random.choice(['+', '-', '*'])
 
+def math_operation(operand1, operand2, operator):
+    """
+    Performs a mathematical operation based on the provided operator.
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+    Parameters:
+    - operand1 (int): The first operand.
+    - operand2 (int): The second operand.
+    - operator (str): The operator ('+', '-', '*').
+
+    Returns:
+    Tuple[str, int]: A tuple containing the mathematical problem as a string and the result of the operation.
+    """
+    problem = f"{operand1} {operator} {operand2}"# Create a string representing a mathematical expression using f-string
+    try:
+        if operator == '+':
+            result = operand1 + operand2
+        elif operator == '-':
+            result = operand1 - operand2
+        else:
+            result = operand1 * operand2
+    except Exception as e:
+        print(f"Error performing the math operation: {e}")
+        result = None
+
+    return problem, result
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """
+    Conducts a math quiz game where the user needs to solve random arithmetic problems.
+    """
+    user_score = 0  # Variable to track the user's score
+    total_questions = 3  # Adjusted for better readability
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(total_questions):
+        operand1 = random_integer(1, 10)
+        operand2 = random_integer(1, 5)
+        operator = random_operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        problem, correct_answer = math_operation(operand1, operand2, operator)
+        print(f"\nQuestion: {problem}")
+        
+        try:
+            user_answer = int(input("Your answer: "))
+        except ValueError:
+            print("Invalid input. Please enter a valid integer.")
+            continue
 
-        if useranswer == ANSWER:
+        if user_answer == correct_answer:
             print("Correct! You earned a point.")
-            s += -(-1)
+            user_score += 1
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {correct_answer}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {user_score}/{total_questions}")
 
 if __name__ == "__main__":
     math_quiz()
