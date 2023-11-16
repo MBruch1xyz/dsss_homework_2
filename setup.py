7783d72c5b99efa63d28c2aec53284085ec99edd
MBruch1xyz <134382524+MBruch1xyz@users.noreply.github.com>
2023-11-16 13:17:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,7 @@ setup(
     name='math_quiz',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=[
-        'numpy==1.20.3',
-        'pandas==1.3.3',
-        'matplotlib==3.4.3',
-    ],
+    install_requires=[],
     entry_points={
         'console_scripts': [
             'math_quiz = math_quiz.math_quiz:main',
